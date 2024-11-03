3e9c1c1ac6a1278254b8f3b5216297b1ea285d73
shrncr <loveyawink@gmail.com>
2024-11-03 17:15:37 -0600

    individual thumbnails

---
+++ client/src/components/paintingthumb.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+export default function PaintingThumb(props){
+    const [img, setImg] = useState(props.image);
+    function changeForm(id){
+        props.setFormID(id);
+        console.log(id)
+
+    }
+    return(
+        <div>
+            <img src={img} ></img>
+            <button onClick={() =>changeForm(props.id)}>Edit</button>
+
+        </div>
+    )
+}
